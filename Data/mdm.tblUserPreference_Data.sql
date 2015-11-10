1eea716a5158f11c2ffce62c0c21f23989ba0c5a
xavier <xavier@xavier.com>
2015-11-10 15:04:23 +0100

    Created an entity with no additional options

--- Data/mdm.tblUserPreference_Data.sql
+++ Data/mdm.tblUserPreference_Data.sql
@@ -3,6 +3,6 @@ INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [Prefe
 INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [PreferenceValue], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (4, 4, N'Version', N'3', '2015-11-10 10:01:37.730', 4, '2015-11-10 10:01:46.267', 4)
 SET IDENTITY_INSERT [mdm].[tblUserPreference] OFF
 SET IDENTITY_INSERT [mdm].[tblUserPreference] ON
-INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [PreferenceValue], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (1, 1, N'Model', N'3', '2015-11-10 08:12:20.140', 1, '2015-11-10 10:35:04.820', 1)
-INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [PreferenceValue], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (2, 1, N'Version', N'3', '2015-11-10 08:12:20.163', 1, '2015-11-10 10:35:08.180', 1)
+INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [PreferenceValue], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (1, 1, N'Model', N'5', '2015-11-10 08:12:20.140', 1, '2015-11-10 13:33:44.977', 1)
+INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [PreferenceValue], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (2, 1, N'Version', N'', '2015-11-10 08:12:20.163', 1, '2015-11-10 13:33:45.017', 1)
 SET IDENTITY_INSERT [mdm].[tblUserPreference] OFF
