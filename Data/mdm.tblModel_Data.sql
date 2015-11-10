aa230a05babb4e53f64139a8327af0f40692532c
xavier <xavier@xavier.com>
2015-11-10 14:19:35 +0100

    Added a model for deletion

--- Data/mdm.tblModel_Data.sql
+++ Data/mdm.tblModel_Data.sql
@@ -1,5 +1,5 @@
 SET IDENTITY_INSERT [mdm].[tblModel] ON
-INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (6, '6b256667-57e6-4309-951a-05d1f0b5b952', N'ModelAndEntity', 0, '2015-11-10 12:02:54.760', 1, '2015-11-10 12:02:54.760', 1)
+INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (7, '1a85d671-27ec-4b0b-8333-0bbf35c688f7', N'ModelDelete', 0, '2015-11-10 13:17:33.167', 1, '2015-11-10 13:17:33.167', 1)
 SET IDENTITY_INSERT [mdm].[tblModel] OFF
 SET IDENTITY_INSERT [mdm].[tblModel] ON
 INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (1, '199be2ea-d7db-4088-a8c6-b6ff9601c8df', N'Metadata', 1, '2015-11-09 18:33:58.863', 1, '2015-11-09 18:33:58.863', 1)
@@ -7,4 +7,5 @@ INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [Ent
 INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (3, '204e7029-2e14-4d7f-a041-d3c009bc1f57', N'ProductSample', 0, '2015-11-10 06:00:57.557', 1, '2015-11-10 06:00:57.557', 1)
 INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (4, 'f1288e09-c8f0-4b1d-b6dd-662270308884', N'ChartOfAccountsSample', 0, '2015-11-10 06:02:03.550', 1, '2015-11-10 06:02:03.550', 1)
 INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (5, '84ea710d-da0b-4300-bf97-0911d96c7510', N'ModelOnly', 0, '2015-11-10 11:48:38.767', 1, '2015-11-10 11:48:38.767', 1)
+INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (6, '6b256667-57e6-4309-951a-05d1f0b5b952', N'ModelAndEntity', 0, '2015-11-10 12:02:54.760', 1, '2015-11-10 12:02:54.760', 1)
 SET IDENTITY_INSERT [mdm].[tblModel] OFF
