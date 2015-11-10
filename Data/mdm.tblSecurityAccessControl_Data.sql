4e618d0029e68432bbc0755d36faa820a6d6b9be
xavier <xavier@xavier.com>
2015-11-10 10:42:30 +0100

    give testgroup access on models: product -> read, customer -> deny, charts -> update

--- Data/mdm.tblSecurityAccessControl_Data.sql
+++ Data/mdm.tblSecurityAccessControl_Data.sql
@@ -1,3 +1,6 @@
 SET IDENTITY_INSERT [mdm].[tblSecurityAccessControl] ON
+INSERT INTO [mdm].[tblSecurityAccessControl] ([ID], [PrincipalType_ID], [Principal_ID], [Role_ID], [Description], [Status_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID], [MUID]) VALUES (2, 2, 2, 2, N'WIN-3L7BE8O8SO5\test Group', 1, '2015-11-10 09:20:45.590', 1, '2015-11-10 09:20:45.590', 1, 'ed64a570-75d3-4d9c-934b-ef243841630d')
+SET IDENTITY_INSERT [mdm].[tblSecurityAccessControl] OFF
+SET IDENTITY_INSERT [mdm].[tblSecurityAccessControl] ON
 INSERT INTO [mdm].[tblSecurityAccessControl] ([ID], [PrincipalType_ID], [Principal_ID], [Role_ID], [Description], [Status_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID], [MUID]) VALUES (1, 1, 1, 1, N'Administrator User', 1, '2015-11-09 18:33:59.120', 1, '2015-11-09 18:33:59.120', 1, '0790f6be-3b52-42f7-bb4d-d559c8b28dca')
 SET IDENTITY_INSERT [mdm].[tblSecurityAccessControl] OFF
--- Data/mdm.tblSecurityRole_Data.sql
+++ Data/mdm.tblSecurityRole_Data.sql
@@ -1,3 +1,6 @@
 SET IDENTITY_INSERT [mdm].[tblSecurityRole] ON
+INSERT INTO [mdm].[tblSecurityRole] ([ID], [MUID], [Name], [Description], [Status_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (2, 'ceb043c7-4014-4b85-be50-61284f46feee', N'Role for Group WIN-3L7BE8O8SO5\test', NULL, 1, '2015-11-10 09:20:45.590', 1, '2015-11-10 09:20:45.590', 1)
+SET IDENTITY_INSERT [mdm].[tblSecurityRole] OFF
+SET IDENTITY_INSERT [mdm].[tblSecurityRole] ON
 INSERT INTO [mdm].[tblSecurityRole] ([ID], [MUID], [Name], [Description], [Status_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (1, '658e5870-bdd6-450e-9c1f-0eb66f706339', N'Role for User Administrator', NULL, 1, '2015-11-09 18:33:59.120', 1, '2015-11-09 18:33:59.120', 1)
 SET IDENTITY_INSERT [mdm].[tblSecurityRole] OFF
