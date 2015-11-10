4b80fdaa654756487db575303eeafa7a46380405
xavier <xavier@xavier.com>
2015-11-10 09:38:28 +0100

    added group test

--- Data/mdm.tblUserGroup_Data.sql
+++ Data/mdm.tblUserGroup_Data.sql
@@ -1,3 +1,6 @@
 SET IDENTITY_INSERT [mdm].[tblUserGroup] ON
+INSERT INTO [mdm].[tblUserGroup] ([ID], [MUID], [UserGroupType_ID], [Status_ID], [SID], [Name], [Description], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (2, 'a999e596-cf4f-4ba9-81b1-a32ea9987fc1', 3, 1, N'S-1-5-21-1658259601-1041775331-2857451580-1010', N'WIN-3L7BE8O8SO5\test', N'', '2015-11-10 08:37:21.797', 1, '2015-11-10 08:37:21.797', 1)
+SET IDENTITY_INSERT [mdm].[tblUserGroup] OFF
+SET IDENTITY_INSERT [mdm].[tblUserGroup] ON
 INSERT INTO [mdm].[tblUserGroup] ([ID], [MUID], [UserGroupType_ID], [Status_ID], [SID], [Name], [Description], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (1, 'e207bdae-a153-417c-a64a-e2293062f828', 3, 1, N'S-1-5-21-1658259601-1041775331-2857451580-1009', N'WIN-3L7BE8O8SO5\mdsexplorers', N'', '2015-11-10 08:14:00.707', 1, '2015-11-10 08:14:00.707', 1)
 SET IDENTITY_INSERT [mdm].[tblUserGroup] OFF
