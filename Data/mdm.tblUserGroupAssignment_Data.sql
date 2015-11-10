abf8ece9d885dae750b25b64fee71ac6b2872fa6
xavier <xavier@xavier.com>
2015-11-10 09:36:14 +0100

    signed explorer2 in for the first time

--- Data/mdm.tblUserGroupAssignment_Data.sql
+++ Data/mdm.tblUserGroupAssignment_Data.sql
@@ -1,3 +1,6 @@
 SET IDENTITY_INSERT [mdm].[tblUserGroupAssignment] ON
+INSERT INTO [mdm].[tblUserGroupAssignment] ([ID], [MUID], [UserGroup_ID], [User_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (2, '9f8c6739-578f-4cbb-a41d-3a08ad1abb8e', 1, 3, '2015-11-10 08:30:18.240', 3, '2015-11-10 08:30:18.240', 3)
+SET IDENTITY_INSERT [mdm].[tblUserGroupAssignment] OFF
+SET IDENTITY_INSERT [mdm].[tblUserGroupAssignment] ON
 INSERT INTO [mdm].[tblUserGroupAssignment] ([ID], [MUID], [UserGroup_ID], [User_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (1, 'f14af5a1-7c0b-42c2-8ff0-c0ebdd28a56e', 1, 2, '2015-11-10 08:20:38.977', 2, '2015-11-10 08:20:38.977', 2)
 SET IDENTITY_INSERT [mdm].[tblUserGroupAssignment] OFF
