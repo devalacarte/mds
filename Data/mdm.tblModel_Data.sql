122638926a169bd11ffdbe38efa69d0b20029931
xavier <xavier@xavier.com>
2015-11-10 14:16:09 +0100

    added a new model with the option to add a new entity of the same name

--- Data/mdm.tblModel_Data.sql
+++ Data/mdm.tblModel_Data.sql
@@ -1,9 +1,10 @@
 SET IDENTITY_INSERT [mdm].[tblModel] ON
-INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (5, '84ea710d-da0b-4300-bf97-0911d96c7510', N'ModelOnly', 0, '2015-11-10 11:48:38.767', 1, '2015-11-10 11:48:38.767', 1)
+INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (6, '6b256667-57e6-4309-951a-05d1f0b5b952', N'ModelAndEntity', 0, '2015-11-10 12:02:54.760', 1, '2015-11-10 12:02:54.760', 1)
 SET IDENTITY_INSERT [mdm].[tblModel] OFF
 SET IDENTITY_INSERT [mdm].[tblModel] ON
 INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (1, '199be2ea-d7db-4088-a8c6-b6ff9601c8df', N'Metadata', 1, '2015-11-09 18:33:58.863', 1, '2015-11-09 18:33:58.863', 1)
 INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (2, '73170a9b-b2d2-47ff-a0d6-d6ae2f8f2ebd', N'CustomerSample', 0, '2015-11-10 05:59:25.420', 1, '2015-11-10 05:59:25.420', 1)
 INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (3, '204e7029-2e14-4d7f-a041-d3c009bc1f57', N'ProductSample', 0, '2015-11-10 06:00:57.557', 1, '2015-11-10 06:00:57.557', 1)
 INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (4, 'f1288e09-c8f0-4b1d-b6dd-662270308884', N'ChartOfAccountsSample', 0, '2015-11-10 06:02:03.550', 1, '2015-11-10 06:02:03.550', 1)
+INSERT INTO [mdm].[tblModel] ([ID], [MUID], [Name], [IsSystem], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (5, '84ea710d-da0b-4300-bf97-0911d96c7510', N'ModelOnly', 0, '2015-11-10 11:48:38.767', 1, '2015-11-10 11:48:38.767', 1)
 SET IDENTITY_INSERT [mdm].[tblModel] OFF
