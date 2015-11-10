745df5c650a5167bd6397a5aacfe8345b752bc7b
xavier <xavier@xavier.com>
2015-11-10 11:29:44 +0100

    login with user from group test for the first time

--- Data/mdm.tblUserGroupAssignment_Data.sql
+++ Data/mdm.tblUserGroupAssignment_Data.sql
@@ -1,6 +1,7 @@
 SET IDENTITY_INSERT [mdm].[tblUserGroupAssignment] ON
-INSERT INTO [mdm].[tblUserGroupAssignment] ([ID], [MUID], [UserGroup_ID], [User_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (2, '9f8c6739-578f-4cbb-a41d-3a08ad1abb8e', 1, 3, '2015-11-10 08:30:18.240', 3, '2015-11-10 08:30:18.240', 3)
+INSERT INTO [mdm].[tblUserGroupAssignment] ([ID], [MUID], [UserGroup_ID], [User_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (3, 'd32a8c65-5a5f-4f41-b9cc-142bb536dbe4', 2, 4, '2015-11-10 10:01:37.330', 4, '2015-11-10 10:01:37.330', 4)
 SET IDENTITY_INSERT [mdm].[tblUserGroupAssignment] OFF
 SET IDENTITY_INSERT [mdm].[tblUserGroupAssignment] ON
 INSERT INTO [mdm].[tblUserGroupAssignment] ([ID], [MUID], [UserGroup_ID], [User_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (1, 'f14af5a1-7c0b-42c2-8ff0-c0ebdd28a56e', 1, 2, '2015-11-10 08:20:38.977', 2, '2015-11-10 08:20:38.977', 2)
+INSERT INTO [mdm].[tblUserGroupAssignment] ([ID], [MUID], [UserGroup_ID], [User_ID], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (2, '9f8c6739-578f-4cbb-a41d-3a08ad1abb8e', 1, 3, '2015-11-10 08:30:18.240', 3, '2015-11-10 08:30:18.240', 3)
 SET IDENTITY_INSERT [mdm].[tblUserGroupAssignment] OFF
--- Data/mdm.tblUserPreference_Data.sql
+++ Data/mdm.tblUserPreference_Data.sql
@@ -1,4 +1,8 @@
 SET IDENTITY_INSERT [mdm].[tblUserPreference] ON
+INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [PreferenceValue], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (3, 4, N'Model', N'3', '2015-11-10 10:01:37.717', 4, '2015-11-10 10:01:46.263', 4)
+INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [PreferenceValue], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (4, 4, N'Version', N'3', '2015-11-10 10:01:37.730', 4, '2015-11-10 10:01:46.267', 4)
+SET IDENTITY_INSERT [mdm].[tblUserPreference] OFF
+SET IDENTITY_INSERT [mdm].[tblUserPreference] ON
 INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [PreferenceValue], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (1, 1, N'Model', N'2', '2015-11-10 08:12:20.140', 1, '2015-11-10 08:12:20.140', 1)
 INSERT INTO [mdm].[tblUserPreference] ([ID], [User_ID], [PreferenceName], [PreferenceValue], [EnterDTM], [EnterUserID], [LastChgDTM], [LastChgUserID]) VALUES (2, 1, N'Version', N'2', '2015-11-10 08:12:20.163', 1, '2015-11-10 08:12:20.163', 1)
 SET IDENTITY_INSERT [mdm].[tblUserPreference] OFF
